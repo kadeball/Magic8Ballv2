6f1d6e2c74a50ef48dbceb989283dd0dcd31687d
kadeball <kadeball@iu.edu>
2022-02-06 15:24:38 -0500

    Final Version

--- app/src/main/java/com/bignerdranch/android/magic8ballv2/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/magic8ballv2/MainActivity.kt
@@ -2,37 +2,80 @@ package com.bignerdranch.android.magic8ballv2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import kotlin.random.Random
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+
+
+private const val TAG = "MainActivity"
+private const val KEY_INDEX = "index"
 
 class MainActivity : AppCompatActivity() {
-
     lateinit var ball_text: TextView
 
-    val answers = arrayOf(
-        "Yes",
-        "Prolly",
-        "Don't Ask Again",
-        "I Already Told You",
-        "My Sources Say No",
-        "Stop Asking",
-        "No",
-        "HYFR"
-    )
 
-    lateinit var random: Random
+    private val magic8BallViewModel: Magic8BallViewModel by lazy {
+        ViewModelProviders.of(this).get(Magic8BallViewModel::class.java)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called");
         setContentView(R.layout.activity_main)
 
-        random  = Random
+        val provider: ViewModelProvider = ViewModelProviders.of(this)
+        val magic8BallViewModel = provider.get(Magic8BallViewModel::class.java)
+        Log.d(TAG, "Got a Magical 8 Ball: $magic8BallViewModel")
 
-        ball_text = findViewById(R.id.ball_text);
+        ball_text = findViewById(R.id.ball_text)
+
 
         ball_text.setOnClickListener{
-            ball_text.text = answers[random.nextInt(answers.size)]
-
+            magic8BallViewModel.moveToNext()
+            updateQuestion()
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        Log.i(TAG, "onSaveInstanceState")
+        savedInstanceState.putInt(KEY_INDEX, magic8BallViewModel.currentIndex)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = magic8BallViewModel.currentQuestionText
+        ball_text.setText(questionTextResId)
+    }
+
 }
